7792d76f483ebc1c2a74169748eedd1d2ba004a3
DJPythonSnake <59803423+DJPythonSnake@users.noreply.github.com>
2022-04-12 13:02:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const multer  = require("multer");
+
+const app = express()
+const port = 3000
+
+const dirname = "/home/whiteyaki/nodejs-project/public/"
+let currentdate = new Date()
+
+app.get('/', (req, res) => {
+  res.redirect('/page/')
+})
+
+app.get('/page/', (req, res) => {
+    res.sendFile(dirname + '/index.html')
+})
+
+const storageConfig = multer.diskStorage({
+  destination: (req, file, cb) =>{
+      cb(null, "uploads");
+  },
+  filename: (req, file, cb) =>{
+      cb(null, file.originalname);
+  }
+});
+
+app.use(express.static(__dirname));
+app.use(multer({storage:storageConfig}).single("filedata"));
+app.post("/upload", function (req, res, next) {
+   
+    let filedata = req.file;
+    if(!filedata)
+        res.send("Ошибка при загрузке файла");
+    else
+        res.send("Файл загружен");
+        res.redirect('/page/')
+});
+
+app.use(function (req, res) {
+  res.status(404).send('not found');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+  console.log(currentdate)
+})
